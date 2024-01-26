f81a2490c5fb65b1487f7a6b5ce9e0d3219f61a9
r3n3gad3p3arl <20760527+r3n3gad3p3arl@users.noreply.github.com>
2024-01-26 16:29:24 -0600

    prepare for move to github

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -1,7 +1,9 @@
-{
+{ lib, config, pkgs, ... }: {
   programs.git = {
     userName = "r3n3gad3p3arl";
-    userEmail = "r3n3gad3p3arl@noreply.codeberg.org";
+    userEmail = "20760527+r3n3gad3p3arl@users.noreply.github.com";
     extraConfig = { init.defaultBranch = "main"; };
   };
+
+  home.packages = lib.mkIf config.programs.git.enable (with pkgs; [ git-filter-repo ]);
 }
